2f35c8ba2dc6702e47618eef4b2d30a1f50403ad
rly <rly@lbl.gov>
2024-02-26 15:48:02 +0000

    Update schedule

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -16,7 +16,7 @@ export default function Schedule() {
           <div className="overflow-x-auto overflow-y-auto max-w-[100vw] mt-12 bg-white">
             <div style={{
               gridTemplateColumns: `3.5rem 16rem 16rem 16rem`,
-              gridTemplateRows: `repeat(24, 4rem)`,
+              gridTemplateRows: `repeat(25, 4rem)`,
             }} className="grid grid-flow-col-dense max-w-full gap-2 p-6 text-center">
               <div className="sticky font-bold border-gray-200 border-b-2">Day</div>
               <div className="sticky font-bold border-gray-200 border-b-2">08:30</div>
@@ -41,6 +41,7 @@ export default function Schedule() {
               <div className="sticky font-bold border-gray-200 border-b-2">18:00</div>
               <div className="sticky font-bold border-gray-200 border-b-2">18:30</div>
               <div className="sticky font-bold border-gray-200 border-b-2">19:00</div>
+              <div className="sticky font-bold border-gray-200 border-b-2">19:30</div>
               <div className="sticky font-bold border-gray-200 border-b-2">Into the night</div>
 
               <div className="sticky p-2 row-span-1 font-bold">
@@ -53,7 +54,7 @@ export default function Schedule() {
               </div>
               <div className="p-2 row-span-10">
               </div>
-              <div className="p-2 row-span-10">
+              <div className="p-2 row-span-11">
               </div>
 
               <div className="sticky p-2 row-span-1 font-bold">
@@ -61,10 +62,10 @@ export default function Schedule() {
               </div>
               <div className="p-2 row-span-11">
               </div>
-              <div className="p-2 row-span-10">
+              <div className="p-2 row-span-11">
               </div>
               <div className="p-2 row-span-2 border-black border-2 border-dashed">
-                Meet at location TBD for dinner together
+                Meet at <a href="https://www.google.com/maps/place/O+Lutador/@38.7008545,-9.1842316,17z/data=!3m1!4b1!4m6!3m5!1s0xd1934ae3546d5ed:0x16634adb1df73d1b!8m2!3d38.7008503!4d-9.1816567!16s%2Fg%2F11cs1fll0f?entry=ttu">O Lutador</a> for dinner together
               </div>
 
               <div className="sticky p-2 row-span-1 font-bold">
@@ -94,8 +95,8 @@ export default function Schedule() {
               <div className="p-2 row-span-5 border-black border-2">
                 Hacking
               </div>
-              <div className="p-2 row-span-2 border-black border-2 border-dashed">
-                Go out to dinner together
+              <div className="p-2 row-span-3 border-black border-2 border-dashed">
+                Go out to dinner together at Mercado de Alges
               </div>
 
               <div className="sticky p-2 row-span-1 font-bold">
@@ -122,8 +123,8 @@ export default function Schedule() {
               <div className="p-2 row-span-3 border-black border-2">
                 Wrap-up and closing
               </div>
-              <div className="p-2 row-span-4 border-black border-2 border-dashed">
-                Go to Simons Foundation social
+              <div className="p-2 row-span-5 border-black border-2 border-dashed">
+                Go to Simons Foundation social at Hyatt Regency Lisbon
               </div>
             </div>
           </div>
